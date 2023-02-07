0407b9482bc9cfbc1b4b321af58be5ff8454ffbb
larni <lauren.gerig@gmail.com>
2023-02-07 17:42:16 -0500

    Add condition to game lose message
    Was saying you lost when you tied because i had it set as a default message unless you won
    or specifically tied.  Also cleaned up some of the console messages to try and make the code
    a bit more readable.

--- javascript.js
+++ javascript.js
@@ -5,33 +5,31 @@ function getComputerChoice() {
     return weaponChoice;
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection = getComputerChoice()) {
     playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
     while (playerSelection !== 'rock' && playerSelection !== 'paper' && playerSelection !== 'scissors') {
         playerSelection = prompt("Choose Rock, Paper, or Scissors!").toLowerCase();
     } 
-
-    computerSelection = getComputerChoice();
 
     console.log("Player: " + playerSelection);
     console.log("Computer: " + computerSelection);
 
     if (playerSelection === computerSelection) {
         console.log("It's a tie!");
+        console.log("Player: " + playerCount + " // " + "Computer: " + computerCount);
+        return;
     }
 
     if (playerSelection === 'rock') {
         if (computerSelection === 'paper') {
             computerCount++;
             console.log("You Lose! Paper beats Rock!");
-            console.log("Player: " + playerCount);
-            console.log("Computer: " + computerCount);
+            console.log("Player: " + playerCount + " // " + "Computer: " + computerCount);
         }
         else {
             playerCount++;
             console.log("You Win! Rock beats Scissors!");
-            console.log("Player: " + playerCount);
-            console.log("Computer: " + computerCount);
+            console.log("Player: " + playerCount + " // " + "Computer: " + computerCount);
         }
     }
 
@@ -39,14 +37,12 @@ function playRound(playerSelection, computerSelection) {
         if (computerSelection === 'rock') {
             playerCount++;
             console.log("You Win! Paper beats Rock!");
-            console.log("Player: " + playerCount);
-            console.log("Computer: " + computerCount);
+            console.log("Player: " + playerCount + " // " + "Computer: " + computerCount);
         } 
         else {
             computerCount++
             console.log("You Lose!  Scissors beats Paper!");
-            console.log("Player: " + playerCount);
-            console.log("Computer: " + computerCount);
+            console.log("Player: " + playerCount + " // " + "Computer: " + computerCount);
         }
     }
 
@@ -54,14 +50,12 @@ function playRound(playerSelection, computerSelection) {
         if (computerSelection === 'rock') {
             computerCount++;
             console.log("You Lose! Rock beats Scissors!");
-            console.log("Player: " + playerCount);
-            console.log("Computer: " + computerCount);
+            console.log("Player: " + playerCount + " // " + "Computer: " + computerCount);
         } 
         else {
             playerCount++;
             console.log("You Win! Scissors beats Paper!");
-            console.log("Player: " + playerCount);
-            console.log("Computer: " + computerCount);
+            console.log("Player: " + playerCount + " // " + "Computer: " + computerCount);
         }
     }
 }
@@ -71,10 +65,13 @@ let computerCount = 0;
 
 function game() {
     for (let i=0; i < 5; i++) {
+        console.log("ROUND " + (i + 1))
         playRound();
     }
 
     if (playerCount === computerCount) console.log("It's a tie!");
     if (playerCount > computerCount) console.log("You win!");
-    else console.log("You lose!");
+    if (playerCount < computerCount) console.log("You lose!");
+    playerCount = 0;
+    computerCount = 0;
 }
