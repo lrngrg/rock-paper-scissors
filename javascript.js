0b15d4d301ddd0e2722b830d03027c1d8d016b5b
larni <lauren.gerig@gmail.com>
2023-02-24 15:29:24 -0500

    Clean code and add score keeping text to click event
    I changed all the nested conditional statements to more concise && statements which cleared up
    the code considerably.  I then was able to insert text content of the game score and add
    a conditional to end the game.  Also was able to disable the buttons once the winning score is
    reached.

--- javascript.js
+++ javascript.js
@@ -1,88 +1,43 @@
 
+let computerCount = 0;
+let playerCount = 0;
+
 function getComputerChoice() {
-    let weaponList = ['rock', 'paper', 'scissors'];
-    let weaponChoice = weaponList[Math.floor(Math.random() * weaponList.length)];
-    return weaponChoice;
+    const weaponList = ['rock', 'paper', 'scissors'];
+    const computerChoice = weaponList[Math.floor(Math.random() * weaponList.length)];
+    return computerChoice;
 }
 
-const buttons = document.querySelectorAll('button');
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) return;
+  if (playerSelection === 'rock' && computerSelection === 'paper') computerCount++;
+  if (playerSelection === 'rock' && computerSelection === 'scissors') playerCount++;
+  if (playerSelection === 'paper' && computerSelection === 'scissors') computerCount++;
+  if (playerSelection === 'paper' && computerSelection === 'rock') playerCount++;
+  if (playerSelection === 'scissors' && computerSelection === 'rock') computerCount++;
+  if (playerSelection === 'scissors' && computerSelection === 'paper') playerCount++;
+}
 
+const playerScoreElement = document.querySelector('#player');
+const computerScoreElement = document.querySelector('#computer');
+const resultsElement = document.querySelector('#results');
+
+const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        playRound(button.id, getComputerChoice());
+      playRound(button.id, getComputerChoice());
+      
+      playerScoreElement.textContent = `${playerCount}`;
+      computerScoreElement.textContent = `${computerCount}`;
+     
+      if (playerCount === 5) {
+        resultsElement.textContent = 'You win!';
+        buttons.forEach((button) => button.setAttribute('disabled', ''));
+      }
+      if (computerCount === 5) {
+        resultsElement.textContent = 'You lose, sad';
+        buttons.forEach((button) => button.setAttribute('disabled', ''));
+      }
     });
 });
 
-
-function playRound(playerSelection, computerSelection) {
-
-    let playerCount = 0;
-    let computerCount = 0;
-
-    console.log("Player chose " + playerSelection);
-    console.log("Computer chose " + computerSelection);
-
-    if (playerSelection === computerSelection) {
-        console.log("It's a tie!");
-        console.log("Player: " + playerCount + " // " + "Computer: " + computerCount);
-        return;
-    }
-
-    if (playerSelection === 'rock') {
-        if (computerSelection === 'paper') {
-            computerCount++;
-            console.log("You Lose! Paper beats Rock!");
-            console.log("Player: " + playerCount + " // " + "Computer: " + computerCount);
-        }
-        else {
-            playerCount++;
-            console.log("You Win! Rock beats Scissors!");
-            console.log("Player: " + playerCount + " // " + "Computer: " + computerCount);
-        }
-    }
-
-    if (playerSelection === 'paper') {
-        if (computerSelection === 'rock') {
-            playerCount++;
-            console.log("You Win! Paper beats Rock!");
-            console.log("Player: " + playerCount + " // " + "Computer: " + computerCount);
-        } 
-        else {
-            computerCount++;
-            console.log("You Lose!  Scissors beats Paper!");
-            console.log("Player: " + playerCount + " // " + "Computer: " + computerCount);
-        }
-    }
-
-    if (playerSelection === 'scissors') {
-        if (computerSelection === 'rock') {
-            computerCount++;
-            console.log("You Lose! Rock beats Scissors!");
-            console.log("Player: " + playerCount + " // " + "Computer: " + computerCount);
-        } 
-        else {
-            playerCount++;
-            console.log("You Win! Scissors beats Paper!");
-            console.log("Player: " + playerCount + " // " + "Computer: " + computerCount);
-        }
-    }
-}
-
-playerCount = 0;
-computerCount = 0;
-
-
-
-// function playFiveRounds() {
-//     for (let i=0; i < 5; i++) {
-//         console.log("ROUND " + (i + 1))
-//         playRound();
-//     }
-
-//     if (playerCount === computerCount) console.log("Tie game ¯\_(ツ)_/¯");
-//     if (playerCount > computerCount) console.log("YOU WON THE GAME");
-//     if (playerCount < computerCount) console.log("You lost the game :(");
-
-//     playerCount = 0;
-//     computerCount = 0;
-// }
